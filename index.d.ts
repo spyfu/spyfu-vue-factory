e28ded0457164325d7ad8c9710f2e5afa38ddc7c
Steven Bedard <scottbedard90@gmail.com>
2018-09-29 14:53:51 -0700

    begin fleshing out types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
-import { Component } from 'vue';
+import Vue, { Component } from 'vue';
 
 declare module 'spyfu-vue-factory' {
-    export function factory(options?: Component, state?: object): any;
+    export interface FactoryOptions {
+        Vue?: Vue,
+        components?: any,
+        routes?: any[],
+        stubTransitions?: boolean,
+        sync?: any,
+    }
+
+    export function MountFunction (options?: Component, state?: object): Vue;
+    
+    export function factory(options?: FactoryOptions): MountFunction;
 }
